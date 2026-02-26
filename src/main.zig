9c652d0545144e9cc9fa9650f80486cb7fc55b26
Gabriel Volpini <gabriel.volpini@ideliverthat.com>
2026-02-26 14:55:52 -0300

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -3,14 +3,14 @@ const std = @import("std");
 var stdout_writer = std.fs.File.stdout().writerStreaming(&.{});
 const stdout = &stdout_writer.interface;
 
-var buffer: [1024]u8 = "";
+var buffer: [1024]u8 = undefined;
 var stdin_reader = std.fs.File.stdin().readerStreaming(&buffer);
 const stdin = &stdin_reader.interface;
 
 pub fn main() !void {
     try stdout.print("$ ", .{});
 
-    const command = stdin.takeDelimiter("\n") orelse "";
+    const command: []const u8 = stdin.takeDelimiter('\n') orelse "";
 
     try stdout.print("{s}: command not found\n", .{command});
 }
