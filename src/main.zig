980e300407dbcd256901bdb9a9274f1b498d72c9
Gabriel Volpini <gabrielvolpini@192.168.0.9>
2026-03-01 20:46:59 -0300

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -16,9 +16,19 @@ const Command = enum {
     pub fn transform(str: []const u8) ?Command {
         return std.meta.stringToEnum(Command, str);
     }
+
+    pub fn isBuiltin(str: []const u8) bool {
+        const value = Command.transform(str);
+        return if (value != null) true else false;
+    }
 };
 
 pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+
+    const allocator = arena.allocator();
+
     while (true) {
         try stdout.print("$ ", .{});
 
@@ -32,7 +42,24 @@ pub fn main() !void {
             .echo => try stdout.print("{s}\n", .{arg}),
             .exit => std.process.exit(0),
             .invalid => try stdout.print("{s}: command not found\n", .{input}),
-            .type => if (Command.transform(arg) != null) try stdout.print("{s} is a shell builtin\n", .{arg}) else try stdout.print("{s}: not found\n", .{arg}),
+            .type => try handleType(allocator, arg),
         }
+    }
+}
+
+fn handleType(allocator: std.mem.Allocator, cmd: []const u8) !void {
+    if (Command.isBuiltin(cmd)) return try stdout.print("{s} is a shell builtin\n", .{cmd});
+
+    const path_env = std.posix.getenv("PATH") orelse return;
+    var paths = std.mem.splitAny(u8, path_env, ":");
+
+    while (paths.next()) |path| {
+        const filePath = try std.fmt.allocPrint(
+            allocator,
+            "{s}/{s}",
+            .{ path, cmd },
+        );
+        _ = std.fs.openFileAbsolute(filePath, .{}) catch continue;
+        try stdout.print("-> {s} \n", .{filePath});
     }
 }
