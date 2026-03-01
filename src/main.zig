35bd2e832ebcad6a8500759992cbe3477943e3ca
Gabriel Volpini <gabrielvolpini@192.168.0.9>
2026-03-01 20:53:29 -0300

    codecrafters submit [skip ci]

--- src/main.zig
+++ src/main.zig
@@ -62,5 +62,6 @@ fn handleType(allocator: std.mem.Allocator, cmd: []const u8) !void {
         std.posix.access(filePath, std.posix.X_OK) catch continue;
 
         try stdout.print("{s} is {s} \n", .{ cmd, filePath });
+        break;
     }
 }
